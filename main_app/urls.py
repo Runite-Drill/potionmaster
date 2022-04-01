dcbcb566e26a2c6487f3203868d7e1de7209a409
Utopia <utopiamusicnz@gmail.com>
2022-04-01 15:28:54 -0700

    User auth implemented

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
+
+    #URL for signup
+    path('accounts/signup/', views.signup, name="signup")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save() #save the user to the database
+            login(request, user) #login the user
+            return redirect('home')
+        else:
+            error_message = 'Invalid signup - Please try again.'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
