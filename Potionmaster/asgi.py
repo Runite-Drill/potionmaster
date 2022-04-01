4bd9acc042afd687da02c9e09c0f45d081b3a70e
Utopia <utopiamusicnz@gmail.com>
2022-04-01 10:59:48 -0700

    Initial commit: Django framework

---
+++ Potionmaster/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Potionmaster project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Potionmaster.settings')
+
+application = get_asgi_application()
