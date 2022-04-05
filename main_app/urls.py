0e100d7a3d6d41affaa34040cc9c186183b3cad7
Nicole Broughton <nbrought28@gmail.com>
2022-04-05 07:52:42 -0700

    second attempt

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name="signup"), 
 
     path("potion/index/", views.potion_index, name="potion_index"),
-    path("potion/delete/<int:potion_id>", views.PotionDelete.as_view(), name="potion_delete"),
+    path("potion/delete/<int:pk>", views.PotionDelete.as_view(), name="potion_delete"),
     path("potion/detail/edit/<int:pk>", views.PotionUpdate.as_view(), name="potion_edit"),
     path("potion/detail/<int:pk>", views.potion_detail, name="potion_detail"),
     path("ingredient/index/", views.ingredient_index, name="ingredient_index"),
